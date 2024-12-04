1fba65492fdd7e62fb75466901ec7bf11dd13adb
0mniaa <omniamahmoud1188@gmail.com>
2024-12-04 20:59:44 +0200

    Updating database connection logic

--- create_tables.sql
+++ create_tables.sql
@@ -2,5 +2,7 @@ CREATE TABLE Client (
     Client_id INT PRIMARY KEY,
     Name VARCHAR(100),
     Email VARCHAR(100),
-    Telephone VARCHAR(15)
+    Telephone VARCHAR(15),
+    CONSTRAINT chk_email CHECK (Email LIKE '%@%'),
+    CONSTRAINT chk_telephone CHECK (Telephone LIKE '+%')
 );
---
+++ insert_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Client (Client_id, Name, Email, Telephone)
+VALUES (1, 'Omnia Mahmoud', 'Omnia@mail.com', '+12345678901');
